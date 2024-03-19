e216f797933ef54d3bbd502643141c85b9eef62f
iosif-blezu <115135785+iosif-blezu@users.noreply.github.com>
2024-03-19 13:18:42 +0200

    Modified tab bar

--- Saverly/src/app/(tabs)/_layout.tsx
+++ Saverly/src/app/(tabs)/_layout.tsx
@@ -1,58 +1,113 @@
 import React from 'react';
-import { Tabs } from 'expo-router';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator, BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-  
-const TabIcon = ({ name, focused }) => {
-    const iconName = (() => {
-      switch (name) {
-        case 'Overview':
-          return focused ? 'home' : 'home-outline';
-        case 'Accounts':
-          return focused ? 'card' : 'card-outline';
-        case 'Settings':
-          return focused ? 'settings' : 'settings-outline';
-        case 'Chat':
-          return focused ? 'chatbubble-ellipses' : 'chatbubble-outline';
-        default:
-          return 'alert-circle'; // default icon if route name doesn't match
-      }
-    })();
-  
-    return <Ionicons name={iconName} size={24} color={focused ? '#6C63FF' : '#BEBEC5'} />;
-  };
-  
-  const TabLayout = () => {
-    return (
-      <Tabs>
-        <Tabs.Screen
+import { BottomFabBar } from 'rn-wave-bottom-bar'; // Import your custom tab bar
+import HomeScreen from './Home'; // Import your screen components
+import AccountsScreen from './Accounts';
+import ChatScreen from './Chat';
+import SettingsScreen from './Settings';
+
+
+type TabIconProps = {
+  name: 'Overview' | 'Accounts' | 'Settings' | 'Chat';
+  focused: boolean;
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabIcon: React.FC<TabIconProps> = ({ name, focused }) => {
+  const iconName: any = (() => {
+    switch (name) {
+      case 'Overview':
+        return focused ? 'home' : 'home-outline';
+      case 'Accounts':
+        return focused ? 'card' : 'card-outline';
+      case 'Settings':
+        return focused ? 'settings' : 'settings-outline';
+      case 'Chat':
+        return focused ? 'chatbubble-ellipses' : 'chatbubble-outline';
+      default:
+        return 'alert-circle'; // default icon if route name doesn't match
+    }
+  })();
+
+  return <Ionicons name={iconName} size={24} color={focused ? '#6C63FF' : '#BEBEC5'} />;
+};
+
+const TabLayout: React.FC = () => {
+  return (
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: '#FFFFFF',
+          tabBarActiveBackgroundColor: '#6C63FF',
+          tabBarInactiveTintColor: '#BEBEC5',
+          tabBarStyle: {
+            backgroundColor: '#FFFFFF', // For active tab background
+            
+          },
+        }}
+        tabBar={(props: any) => (
+          <BottomFabBar
+            mode={ 'default'} // Assuming you want the 'square' mode, not 'default'
+            isRtl={false}
+            
+            
+            focusedButtonStyle={{
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 7,
+              },
+              shadowOpacity: 0.41,
+              shadowRadius: 9.11,
+              elevation: 14,
+            }}
+            bottomBarContainerStyle={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              
+            }}
+            {...props}
+          />
+        )}
+      >
+        <Tab.Screen
           name="Home"
+          component={HomeScreen}
           options={{
             headerShown: false,
             tabBarIcon: ({ focused }) => <TabIcon name="Overview" focused={focused} />,
           }}
         />
-        <Tabs.Screen
+        <Tab.Screen
           name="Accounts"
+          component={AccountsScreen}
           options={{
             headerShown: false,
             tabBarIcon: ({ focused }) => <TabIcon name="Accounts" focused={focused} />,
           }}
         />
-        <Tabs.Screen
+        <Tab.Screen
           name="Chat"
+          component={ChatScreen}
           options={{
             headerShown: false,
             tabBarIcon: ({ focused }) => <TabIcon name="Chat" focused={focused} />,
           }}
         />
-        <Tabs.Screen
+        <Tab.Screen
           name="Settings"
+          component={SettingsScreen}
           options={{
             tabBarIcon: ({ focused }) => <TabIcon name="Settings" focused={focused} />,
           }}
         />
-      </Tabs>
-    );
-  };
+      </Tab.Navigator>
+    
+  );
+};
 
 export default TabLayout;
