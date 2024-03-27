179aceda23e909b4311a430e4bcea14bdfd9a034
Mihnea2003 <bro_bigger@yahoo.ro>
2024-03-27 17:33:19 +0200

    butoane

--- Saverly/src/app/(tabs)/Accounts.tsx
+++ Saverly/src/app/(tabs)/Accounts.tsx
@@ -1,13 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { View, Text, StyleSheet, FlatList, RefreshControl, TouchableOpacity, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
 import { fetchUserAccounts } from '../../services/accountService'; // Adjust as necessary
 import { router } from 'expo-router';
-import { FIREBASE_AUTH, FIREBASE_DB } from 'firebaseConfig';
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import PageHeader from '@/components/PageHeader';
-
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import {FIREBASE_AUTH,FIREBASE_DB } from '../../../firebaseConfig';
+import {Animated} from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
@@ -39,7 +38,6 @@ const AccountCard = ({ account }) => (
   </View>
 );
 
-
 const Pagination = ({ index, total }) => {
   let dots = [];
   for (let i = 0; i < total; i++) {
@@ -64,8 +62,46 @@ const AccountsScreen = ({ navigation }) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [loading, setLoading] = useState(false);
   const [selectedAccount, setSelectedAccount] = useState(null);
+  const [areButtonsVisible, setAreButtonsVisible] = useState(false);
+  const [areOptionsVisible, setAreOptionsVisible] = useState(false);
 
   const userId = FIREBASE_AUTH.currentUser?.uid;
+
+  const opacityAnim = useRef(new Animated.Value(0)).current; // For fade-in effect
+  const positionAnim = useRef(new Animated.Value(1)).current;
+
+  const toggleButtonsVisibility = () => {
+    setAreButtonsVisible((prevVisible) => !prevVisible);
+    // Trigger animation based on the new state
+    Animated.timing(opacityAnim, {
+      toValue: areButtonsVisible ? 0 : 1, // Fade in if becoming visible, out if hiding
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(positionAnim, {
+      toValue: areButtonsVisible ? 1 : 0, // Slide up if becoming visible, down if hiding
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const toggleOptionsVisibility = () => {
+    setAreOptionsVisible((prevVisible) => !prevVisible);
+    // Trigger animation based on the new state
+    Animated.timing(opacityAnim, {
+      toValue: areOptionsVisible ? 0 : 1, // Fade in if becoming visible, out if hiding
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(positionAnim, {
+      toValue: areOptionsVisible ? 1 : 0, // Slide up if becoming visible, down if hiding
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   const fetchExpensesForAccount = async (accountId) => {
     try {
       const expensesCollectionRef = collection(FIREBASE_DB, 'users', userId, 'expenses');
@@ -136,8 +172,8 @@ const AccountsScreen = ({ navigation }) => {
       </View>
     );
   };
+
   return (
-    <><PageHeader title="Accounts" />
     <View style={styles.container}>
       <FlatList
         horizontal
@@ -153,17 +189,46 @@ const AccountsScreen = ({ navigation }) => {
         scrollEventThrottle={16}
       />
 
-      {/* Add Account Floating Action Button */}
+      {/* Toggle Button */}
       <TouchableOpacity
-        style={styles.addButton}
-        onPress={() => router.push('AddAccount')}
+        style={styles.actionButton}
+        onPress={toggleButtonsVisibility}
         activeOpacity={0.7}
       >
-        <Ionicons name='add' size={30} color="white" />
+        <Ionicons name="ellipsis-vertical" size={30} color="#fff" />
       </TouchableOpacity>
-      <Pagination index={activeIndex} total={accounts.length} />
+      <Animated.View
+          style={[
+            styles.animatedContainer,
+            {
+              opacity: opacityAnim,
+              transform: [{ translateY: positionAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, -20] // Change based on your UI needs
+              }) }],
+            },
+          ]}
+        >
+          {/* Option 1 Button */}
+          <TouchableOpacity
+            style={styles.optionButton2}
+            onPress={() => router.push('addExpense')}
+            activeOpacity={0.7}
+          >
+            <Ionicons name="alert-circle-outline" size={30} color="#FFF" />
+          </TouchableOpacity>
 
-    </View></>
+          {/* Option 2 Button */}
+          <TouchableOpacity
+            style={styles.optionButton}
+            onPress={() => router.push('AddAccount')}
+            activeOpacity={0.7}
+          >
+            <Ionicons name="card" size={30} color="#FFF" />
+          </TouchableOpacity>
+        </Animated.View>
+      <Pagination index={activeIndex} total={accounts.length} />
+    </View>
   );
 };
 
@@ -171,16 +236,55 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#33404F',
-    paddingTop:45,
   },
   flatListContentContainer: {
     paddingTop: 0, // space from the top of the screen
+  },
+  fab: {
+    position: 'relative',
+    right: (width-56) / 2, // Adjust this value based on your screen width and FAB width (56
+    bottom: 110, // Adjust this value based on your tab bar height
+    backgroundColor: '#B5C5C3', // Use your app's theme color
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 8,
+    shadowColor: '#89CFF3',
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    shadowOffset: { width: 5, height: 5 },
   },
   cardContainer: {
     width: width, // each card spans the full width
     height: CARD_HEIGHT, // set height of the card
     paddingTop: 60, // add padding to the top of the card
     alignItems: 'center',
+  },
+  animatedContainer: {
+    // Container for the animated buttons
+    position: 'relative',
+    right: 120, // Adjust based on your layout
+    bottom: 80, // Adjust based on your layout
+  },
+  actionButton: {
+    position: 'relative', // Position the button over the content
+    right: 0, // 30 pixels from the right edge of the screen
+    left:(width-56)/2,
+    bottom: 110, // 30 pixels from the bottom edge of the screen
+    backgroundColor: '#00DDA3', // A bright color to stand out
+    width: 56, // The diameter of the FAB
+    height: 56, // The diameter of the FAB
+    borderRadius: 28, // Half the size of the width/height to create a circle
+    justifyContent: 'center', // Center the icon horizontally
+    alignItems: 'center', // Center the icon vertically
+    elevation: 6, // Shadow for Android
+    shadowColor: '#000', // Shadow color for iOS
+    shadowOffset: { width: 0, height: 4 }, // Shadow position for iOS
+    shadowOpacity: 0.3, // Shadow opacity for iOS
+    shadowRadius: 4, // Shadow blur radius for iOS
+    zIndex: 10, // Ensure the button stays above other elements
   },
   expenseCard: {
     backgroundColor: '#FFFFFF', // Light background for the card
@@ -225,6 +329,38 @@ const styles = StyleSheet.create({
     color: '#33404F',
     marginTop: 4,
   },
+  optionButton: {
+    position: 'absolute',
+    right: 100, // center the button horizontally
+    bottom: 80,
+    backgroundColor: '#B5C5C3',
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 8,
+    shadowColor: '#6C63FF',
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 5 },
+  },
+  optionButton2: {
+    position: 'absolute',
+    right: 10, // adjust the horizontal position
+    bottom: 80, // adjust the vertical position
+    backgroundColor: '#B5C5C3',
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 8,
+    shadowColor: '#6C63FF',
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 5 },
+  },
   card: {
     backgroundColor: 'white',
     borderRadius: 20,
@@ -252,7 +388,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   addButton: {
-    position: 'absolute',
+    position: 'relative',
     right: (width - 56) / 2, // center the button horizontally
     bottom: 110,
     backgroundColor: '#B5C5C3',
@@ -272,7 +408,6 @@ const styles = StyleSheet.create({
     bottom: 560,
     flexDirection: 'row',
     alignSelf: 'center',
-    paddingTop:45,
   },
   dot: {
     height: 10,
